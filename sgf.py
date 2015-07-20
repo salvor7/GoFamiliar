dff0c563b8de8f843c4406bbd1208326495093d7
Andrew Brown <salvor7@gmail.com>
2015-07-20 07:30:57 -0700

    Add sgf reading and its unit test py files

---
+++ sgf.py
@@ -0,0 +1,90 @@
+import re
+import numpy as np
+from string import ascii_letters
+
+
+size = 19
+branch_start = '('
+branch_end = ')'
+node_marker = ';'
+newline = '\n'
+sgf_move_patt = re.compile(r'[BW][\[][a-s]{2}[\]]')
+player_assign = {'B': 1, 'W': -1}
+letter_coord_id = {letter: coord for letter, coord in zip(ascii_letters, range(size))}
+
+
+def branchreader(sgfstr):
+    """
+
+    :param sgfstr:
+    :return:
+    """
+    if type(sgfstr) == str:
+        sgfchunks = sgfstr.replace(newline, '').split(branch_start)
+    else:
+        sgfchunks = sgfstr
+
+    chunk = sgfchunks[0]
+    game = []
+    if chunk == '':
+        game = branchreader(sgfchunks[1:])
+    else:
+        game += chunk.replace(branch_end, '').split(node_marker)
+        if sgfchunks[1:]:
+            game.append(branchreader(sgfchunks[1:]))
+
+    while '' in game:
+        game.remove('')
+    return game
+
+
+def node_to_board(node):
+    """
+    :param node:
+    :return:
+    """
+    board = np.zeros((size, size), dtype=np.int8)
+
+    try:
+        move = re.findall(sgf_move_patt, node)[0]
+    except IndexError:
+        return board
+
+    player = player_assign[move[0]]
+    x_coord, y_coord = letter_coord_id[move[2]], letter_coord_id[move[3]]
+
+    board[y_coord][x_coord] += player
+
+    return board
+
+
+def nodelist_to_boardlist(nodelist):
+    """
+
+    :param nodelist:
+    :return:
+    """
+    boardlist = [np.zeros((size, size), dtype=np.int8)]
+    for node in nodelist:
+        board = node_to_board(node)
+        boardlist.append(np.sum([boardlist[-1], board], axis=0))
+
+    return np.array(boardlist)
+
+
+def printsgf(branchlist, level=0):
+    """
+
+    :param branchlist:
+    :param level:
+    :return:
+    """
+    for branch in branchlist:
+        if type(branch) == str:
+            print('     ' * level + branch)
+        else:
+            printsgf(branch, level+1)
+
+
+def remove_captures(move_board, board):
+    pass
---
+++ sgf_unit.py
@@ -0,0 +1,34 @@
+__author__ = 'salvor7'
+
+from unittest import TestCase
+import sgf
+
+class SGFReaderTests(TestCase):
+
+    with open(r'test2.sgf', encoding="utf8") as file:
+        linear_sgf = file.read()
+
+    with open(r'testsgf.sgf', encoding="utf8") as file:
+        branchy_sgf = file.read()
+
+    def test_branchreader(self):
+        def diveintobranches(branchlist):
+            assert '' not in branchlist
+
+            for node in branchlist:
+                if type(node) == list:
+                    diveintobranches(node)
+        length_of_linear_sgf = 285
+        length_of_branchy_sgf = 12
+
+        assert len(sgf.branchreader(self.linear_sgf)) == length_of_linear_sgf
+        assert len(sgf.branchreader(self.branchy_sgf)) == length_of_branchy_sgf
+
+    def test_node_to_board(self):
+        assert sgf.node_to_board(r'B[dd]')[3][3] == 1
+        assert sgf.node_to_board(r'W[sa]')[0][18] == -1
+
+    def test_nodelist_to_boardlist(self):
+        branchlist = sgf.branchreader(self.linear_sgf)
+        print(len(sgf.nodelist_to_boardlist(branchlist)))
+        assert len(branchlist) == len(sgf.nodelist_to_boardlist(branchlist))
