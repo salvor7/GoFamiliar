e08be6c5ff2fe5502f469fc627fa37e0b1910e55
Andrew Brown <salvor7@gmail.com>
2015-08-07 22:40:00 -0700

    move go_heatmap to analysis module
    Former-commit-id: d1f7ea9a1f87cb036bddb905ff66f82a1cb27ec7

--- go_heatmap.py
+++ go_heatmap.py
@@ -1,17 +1,2 @@
-from bokeh.charts import HeatMap, show, output_file
-from bokeh.palettes import Spectral11 as palette
 
-
-def make_heatmap(counts_df, file_name='ghm.html'):
-
-    output_file(file_name)
-    show(HeatMap(counts_df, palette=palette))
-
-
-# DO LATER find if colours can be transparent
-# DO LATER find if a background png can be added to the display ie a goban
-# DO LATER create a user defined palettes
-# DO LATER pixelate the heatmap     pixels = 7
-
-# DO LATER test how updating works
 
