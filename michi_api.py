dc50a042fc9e66efa12e7579656e9b3414f5b760
Andrew Brown <salvor7@gmail.com>
2015-08-07 20:43:11 -0700

    Add docstring to michi_api
    Former-commit-id: 5a41229d7b88787600a6d9115a9cb3494165d4bf

--- michi_api.py
+++ michi_api.py
@@ -1,3 +1,12 @@
-#TODO fix michi importing
-import michi
+"""
+This module contains functions and objects required to use the michi Monte Carlo Go Game simulator. This will include
+    - starting and stopping the simulator
+    - capturing, storing, filtering and managing simulated games
+    - providing simulated games
+    - providing move suggestions
+    - accepting interaction such as seed and what if moves
+    - handling any multi-threading of michi instances
+"""
+__author__ = 'Andrew AH Brown'
 
+import michi
