c4ebf5e800767faaef959e8d5d8147bc8af898d7
Andrew Brown <salvor7@gmail.com>
2015-08-10 22:38:37 -0700

    create GoMove class
    Former-commit-id: e680826ebe78888ebef5a3a99d50ea892a8a9042

---
+++ gomove.py
@@ -0,0 +1,11 @@
+"""This file defines the GoMove object."""
+from collections import namedtuple
+
+class GoMove(namedtuple('Move', 'player x y')):
+    """
+
+    :param colour: 1 or -1  for black or white
+    :param x: int  1 to 19
+    :param x: int  1 to 19
+    """
+    pass
