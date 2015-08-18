73b13e5a8cd6f7c4c0ef7db5c40cc70d843f8776
salvor7 <salvor7@gmail.con>
2015-08-17 19:23:11 -0700

    update docstrings and gomove has relation
    Former-commit-id: c9e50e4644c623c08dda8dcb6e3788423a2906e8

--- gomove.py
+++ gomove.py
@@ -1,11 +1,26 @@
-"""This file defines the GoMove object."""
+"""This file defines the GoMove object.
+
+>>> move = GoMove(1, 16, 4)
+>>> move.player
+1
+>>> move.x, move.y
+16, 4
+"""
 from collections import namedtuple
 
-class GoMove(namedtuple('Move', 'player x y')):
+GoMove = namedtuple('Move','player x y')
+
+class GoMove2(namedtuple('Move', 'player x y')):
     """
 
     :param colour: 1 or -1  for black or white
     :param x: int  1 to 19
     :param x: int  1 to 19
     """
-    pass
+    
+    def __init__(self, player, x, y):
+      """
+      GoMove constructor.
+      
+      
+      """
--- goposition.py
+++ goposition.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 class GoPosition(np.ndarray):
-    """
+    """GoPosition object.
+    
     The size (k) of the game board is set at object construction, can be any odd integer and is based.
 
     Any game state is recorded as a k by k np.array with the entries
@@ -18,7 +19,9 @@ class GoPosition(np.ndarray):
     default_size = 19
 
     def __init__(self, moves=None, size=default_size, komi=6.5):
-        """The GoPosition object can be constructed as a blank board 19 by 19 (default)
+        """GoPosition constructor.
+        
+        The GoPosition object can be constructed as a blank board 19 by 19 (default)
         or using a list of GoMoves.
 
         >>> print(GoPosition(size=3))
@@ -40,6 +43,8 @@ class GoPosition(np.ndarray):
 
     def add_move(self, move):
         """
+        Return a GoPosition post adding a move to self.
+        
         Add a new k by k board representing the game state after the player move, including removing captures, and
         updating self.last_move with move.
 
