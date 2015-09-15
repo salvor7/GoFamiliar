e517a1b069748db5933d5b96bcb2ed7d98e59ef2
Andrew Brown <salvor7@gmail.com>
2015-09-14 21:29:35 -0700

    Create single csv file of all pro game sgf files
    Former-commit-id: a2d001fa7a646582b7bdc3511f5e5eb508d4a6a0

--- createSGFcsv.py
+++ createSGFcsv.py
@@ -1,8 +1,17 @@
 import util.directory_tools as dt
 
-dir = 'C:\AllProgrammingProjects\GoFamiliar\sgf_store\baduk-pro-collection'
+dir = u'C:/AllProgrammingProjects/GoFamiliar/sgf_store/baduk-pro-collection'
 
-with open('all_sgfs.csv', 'r'):
+with open('sgf_store/all_sgfs.csv', 'w', encoding='utf-8') as csv_file:
+    files_found = dt.search_tree(directory=dir, file_sig='*.sgf')
 
-    for file in dt.search_tree(directory=dir, file_sig='*.sgf'):
-        with open(file) as sgf_:
+    for file in files_found:
+        with open(file, errors='replace', encoding='utf-8') as sgf_file:
+
+            try:
+                string = sgf_file.read().replace('\n', '')
+            except:
+                print(file)
+                raise
+            if 'SZ[19]' in string:
+                csv_file.writelines(string + '\n')
