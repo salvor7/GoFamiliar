95c5d5ca054d929bf79631b0fd2a18da1a05c9e1
Andrew Brown <salvor7@gmail.com>
2015-09-27 13:14:33 -0700

    Completed extensive debugging
    Run the parser on the entire sgf_store/baduk-pro-collection folder.
    Revealed several problem with the parsing methods used.
    sgf_parser was extensiving changed with new replacements being made and a careful ordering of the operations.
    Error handling in several functions was improved to aid in finding malformed sgf files in future.
    
    
    Former-commit-id: 92d72db3df3dee8da8ae55a312d7d9510d9c40e2

--- sgf.py
+++ sgf.py
@@ -14,6 +14,7 @@ import util.directory_tools as dt
 from go_objects import GoMove
 
 # regex pattern found at at http://www.nncron.ru/help/EN/add_info/regexp.htm Operators section
+sgf_move_patt = re.compile(r'[BW]\[[a-s][a-s]\]')
 sgf_info_patt = re.compile(r'([A-Z][A-Z]?)\[(.+?)\]')
 
 def sgf_parser(sgf_str):
@@ -26,8 +27,8 @@ def sgf_parser(sgf_str):
     Regex replacement format found at https://docs.python.org/2/howto/regex.html#modifying-strings
 
     Square braces are replaced with angled so that they do not interact with the literal_eval.
-    KM[6.5]  ->     "KM[6.5]",
-    B[de]    ->     "B[de]",
+    KM[6.5]  ->     r"KM[6.5]",
+    B[de]    ->     r"B[de]",
     The remaining replacements in new_chars were discovered by investigating smart game formatting
     and trial and error.
 
@@ -40,13 +41,13 @@ def sgf_parser(sgf_str):
     >>> basic_branching1 = '(;SZ[19](;B[qd];W[dd];B[oc])(;B[do];W[dq]))'
     >>> sgf_parser(basic_branching1)
     ['SZ[19]', ['B[qd]', 'W[dd]', 'B[oc]'], ['B[do]', 'W[dq]']]
-    >>> basic_branching2 = '(;SZ[19];B[jj];W[kl](;B[dd](;W[gh])(;W[sa]))(;B[cd]))'
+    >>> basic_branching2 = '(;SZ[19];B[jj];W[kl](;B[dd](;W[gh])   (;W[sa]))(;B[cd]))'
     >>> sgf_parser(basic_branching2)
     ['SZ[19]', 'B[jj]', 'W[kl]', ['B[dd]', ['W[gh]'], ['W[sa]']], ['B[cd]']]
     >>> complex_branching = ('(;RU[Japanese]SZ[19]KM[6.50];B[jj];W[kl]'
-    ...                 '(;B[pd](;W[pp])(;W[dc](;B[de])(;B[dp])))'
+    ...                 '(;B[pd](;W[pp]) (;W[dc](;B[de])(;B[dp])))'
     ...                 '(;B[cd];W[dp])'
-    ...                 '(;B[cq](;W[pq])(;W[pd]))'
+    ...                 '(;B[cq](;W[pq])  (;W[pd]))'
     ...                 '(;B[oq];W[dd]))')
     >>> for chunk in sgf_parser(complex_branching):
     ...    chunk
@@ -63,21 +64,32 @@ def sgf_parser(sgf_str):
     >>> game = sgf_parser(branchy_sgf)
 
     """
+    bad_chars = [('\n', '')         # get rid of newline
+                 , ('][', ' ')      # combine sequence (AB, AW, LB) info into one item
+                 , ('"', '')        # double quotes used below
+                ]
+    for bad, fix in bad_chars:      # clear all round braces in sgf info nodes
+        sgf_str = sgf_str.replace(bad, fix)
 
     # regex replacement format found at https://docs.python.org/2/howto/regex.html#modifying-strings
-    sgf_str = sgf_info_patt.sub(r'"\1[\2]",',sgf_str)
+    sgf_str = sgf_info_patt.sub(r'r"\1[\2]",', sgf_str)     # add double quotes and raw string r
 
-    new_chars = [('\n', ''),        # get rid of newline
-                 (';', ''),         # get rid of colons
+    new_chars = [(';', ''),         # get rid of colons
                  ('(', '['),        # change tuple braces to list braces
                  (')', ']'),
-                 ('][', '], [')     # add comma between branches
                  ]
 
     for char, new in new_chars:
         sgf_str = sgf_str.replace(char, new)
 
-    return ast.literal_eval(sgf_str)
+    sgf_str = re.sub(r'(\]) *(\[)', r'\1, \2', sgf_str)  # add comma between branches
+
+    try:
+        return ast.literal_eval(sgf_str)
+    except Exception as err:
+        message = str(err) + '\n' + sgf_str
+        raise SGFError(message)
+
 
 
 def sgf_main_branch(sgf_list):
@@ -124,10 +136,9 @@ def node_to_move(node):
     GoMove(player=-1, x=16, y=17)
     >>> try:
     ...     node_to_move('error')
-    ... except TypeError as err:
+    ... except ValueError as err:
     ...     print(err)
-    node is not Smart Game formatted
-
+    "error" is not a sgf move formatted node
 
     :param node: SGF Node eg B[ah]
     :return: GoMove
@@ -137,12 +148,10 @@ def node_to_move(node):
     player_assign = {'B': 1, 'W': -1}
     # found regex patterns at http://www.nncron.ru/help/EN/add_info/regexp.htm Operators section
 
-    sgf_move_patt = re.compile(r'[BW]\[[a-s][a-s]\]')
-
     try:
         move = re.findall(sgf_move_patt, node)[0]
     except IndexError:
-        raise TypeError('node is not Smart Game formatted')
+        raise ValueError('"' + node + '" is not a sgf move formatted node')
 
     player = player_assign[move[0]]
     x_coord, y_coord = letter_coord_id[move[2]], letter_coord_id[move[3]]
@@ -159,7 +168,11 @@ def sgf_info(attribute):
     :param attribute: string
     :return: string, string
     """
-    name, value = attribute.replace(']', '').split('[')
+    try:
+        name, value = re.findall(sgf_info_patt, attribute)[0]
+    except BaseException as err:
+        message = '"' + attribute + '" ' + 'is not a sgf info formatted node.'
+        raise ValueError(message)
     return name, value
 
 
@@ -198,23 +211,30 @@ def sgf_store():
     dir = u'C:/AllProgrammingProjects/GoFamiliar/sgf_store/baduk-pro-collection'
 
     files_found = dt.search_tree(directory=dir, file_sig='*.sgf')
-
+    bad_files = []
     for file in files_found:
-        with open(file, errors='replace', encoding='utf-8') as sgf_file:
+        with open(file, errors='ignore', encoding='utf-8') as sgf_file:
+
+            string = sgf_file.read()
 
             try:
-                string = sgf_file.read()
-            except:
-                print(file)
-                raise
-            yield sgf_main_branch(string)
+                yield sgf_main_branch(sgf_parser(string))
+            except Exception as err:
+                message = str(err) + '\n' + file + '\n' + string + '\n'*2
+                message = message.encode('utf-8', errors='ignore').decode(encoding='ascii', errors='ignore')
+                bad_files.append(message)
 
+    for msg in bad_files:
+        print(msg)
+
+
+
+class SGFError(Exception):
+    pass
 
 
 def create_sgf_hdf5(file='pro_collection.hdf5', dir='sgf_store/'):
     """Create hdf5 file of sgf_store
-
-    >>> create_sgf_hdf5()
 
     :param file: string
     :param dir: string
@@ -230,11 +250,16 @@ def create_sgf_hdf5(file='pro_collection.hdf5', dir='sgf_store/'):
         for node in sgf_nodes:
             try:
                 move_list.append(node_to_move(node))
-            except TypeError:
+            except ValueError:
                 name, value = sgf_info(node)
-                pro_games[curr_game].attrs[name.encode('UTF8')] = value.encode('UTF8')
+                if value == '' or value == ' ':     # don't record blank info
+                    continue
+                if name == 'C':
+                    name += str(len(move_list))     # associated comment to specific move
+                pro_games[curr_game].attrs[name] = value
 
         pro_games[curr_game].create_dataset('moves', data=np.array(move_list))
+
 
 if __name__ == '__main__':
     create_sgf_hdf5()
