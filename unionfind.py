4a37fb7dbe64dee1550cc2a27e6be1bf35fe3a63
salvor7 <salvor7@gmail.con>
2015-12-09 08:53:53 -0800

    finish union functions of unionfind

--- unionfind.py
+++ unionfind.py
@@ -1,19 +1,24 @@
 import numpy as np
 
 class UnionFind():
-    def __init__(self, leadertype, size=None):
+    def __init__(self, size=None):
         self.pointers = np.array([range(size)])
         
     def __getitem__(self, elem):
         if self.pointers[elem] == elem:
             return elem
         else:
-            repre = self[self.pointers[elem]]
-            self.point[elem] = repre
+            repre = self.__getitem__(self.pointers[elem])
+            self.pointers[elem] = repre
             return repre
             
      def __iter__(self):
          return iter(self.pointers)
      
-     def union(self, set1, set2):
+     def union(self, repre1, repre2):
+         self.pointers[repre2] = repre1
          
+     def multi_union(self, repre_iter):
+         repre1 = next(repre_iter)
+         for repre_n in repre_iter:
+             self.union(repre1, repre_n)
