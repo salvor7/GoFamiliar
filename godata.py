e0a8152a4bd7e2b5a71f07c26726d68664ff0818
Andrew Brown <salvor7@gmail.com>
2015-12-09 21:59:08 -0800

    Precalculate coordinate neighbors
    NEIGHBORS dictionary is a store of coordinate neighbors which is
    precalculated to save on calculating neighbors everytime.

--- godata.py
+++ godata.py
@@ -1,6 +1,41 @@
 from unionfind import UnionFind
+import numpy as np
 
-NEIGHBORS = {}
+
+def make_neighbors(size=19):
+    """Generator of neighbors coordinates
+
+    In general, [c+1, c-1, c+size, c-size] are the coordinates of neighbors of a flatten board.
+    Special care must be taken for the edges, and the 4 if statements limit neighbors for
+    coordinates along the edge.
+    Neighbor checking is the most common function in the MCTS, so we have used np.array for a
+    speed boost.
+
+    :param size: int for board size
+    :yield: int, np.array  for coordinate and array of neighbors.
+    """
+    for c in range(size**2):
+        if c < size:
+            up = []
+        else:
+            up = [c - 1]
+        if c >= size*(size - 1):
+            down = []
+        else:
+            down = [c + 1]
+        if c % size == 0:
+            left = []
+        else:
+            left = [c - size]
+        if (c + 1) % size == 0:
+            right = []
+        else:
+            right = [c + size]
+
+        yield c, np.array(up+down+left+right)
+
+NEIGHBORS = {c:neigh for c, neigh in make_neighbors()}
+
 
 class Position():
     def __init__(self, size=19):
