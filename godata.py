7c07339bb086173ef1e1d8c148524da6c04f293f
Andrew Brown <salvor7@gmail.com>
2015-12-09 21:17:59 -0800

    Select go data structure object name as Position
    Decided between Board and Position. Position won out, and the
    functionality was combined.

--- godata.py
+++ godata.py
@@ -1,28 +1,17 @@
 from unionfind import UnionFind
 
-
-class Board():
-
-    def __init__(self, size):
-        self.points = UnionFind()
-        self.pounts[size**2 - 1]
-        self.groups = {}
-        
-    def __iter__(self):
-        return iter(self.points)
-    
-    def move(self):
-        
-    
 NEIGHBORS = {}
 
 class Position():
     def __init__(self, size=19):
-        self.board = [i for i in range(size**2)]
+        self.board = UnionFind(size=size**2)
+        self.groups = {}
         self.ko = None
         self.size = size
     
-    def move(self, c):
+    def move(self, pt):
         if self.ko is not None:
-            raise MoveError(str(c) + 'is a ko point')
-        [NEIGHBORS[c]]
+            raise MoveError(str(pt) + 'is a ko point')
+
+    def __iter__(self):
+        return iter(self.board)
