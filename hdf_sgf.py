bdedc852e58b2c84bfccc85d9639e09668bc94fb
Andrew Brown <salvor7@gmail.com>
2015-12-09 20:00:46 -0800

    Load the hdf5 pro game file

--- hdf_sgf.py
+++ hdf_sgf.py
@@ -1,12 +1,24 @@
 import h5py
 from os import path
+import numpy as np
 
-file = 'sgfhdf5_doctest.hdf5'
+file = 'pro_collection.hdf5'
 fpath = 'sgf_store'
 
 pro_games = h5py.File(path.join(fpath, file), 'r')
 
-for key in pro_games:
-    game = pro_games[key]
-    attrs = game.attrs
-    print(game, len(attrs))
+#access moves
+# m = 0
+# for idx, key in enumerate(pro_games):
+#     m += len(pro_games[key]['moves'])
+# print(m)
+
+print(len(pro_games['19']))
+#access size 19 games
+count_board = np.zeros(shape=(19,19), dtype=np.int)
+for idx, game in enumerate(pro_games['19']):
+
+    player, x, y = pro_games['19'][game][0]
+    count_board[x-1, y-1] += 1
+
+print(count_board)
