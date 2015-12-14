fee628aea76351afafd26ea7508f258d3280ae5f
Andrew Brown <salvor7@gmail.com>
2015-12-13 16:43:18 -0800

    Use Position fixture for neighbor test
    The loop will be executed by the fixture

--- test_godata.py
+++ test_godata.py
@@ -5,34 +5,31 @@ import pytest
 def position(request):
     return gd.Position(size=request.param)
 
-def test_make_neighbors():
 @pytest.fixture(scope='module')
 def position_moves(position):
     size = position.size
     black_stones = [1, size, 1+size*2]
+
+def test_make_neighbors(position):
     """Test neighbor making by counting occurrences of neighbors
 
     Corners have two neighbors; Edges points have 3, and Centre points have 4.
     """
     def result_row(i, size):
         return [i] + [i+1]*(size-2) + [i]
-    try:
-        next(make_neighbors(0))
-    except StopIteration:
-        pass
-    next(make_neighbors(1))
 
-    for size in range(2, 23):
-        neigh_counts = [0]*(size**2)
-        first_row = result_row(2, size)
-        last_row = result_row(2, size)
-        middle_row = result_row(3, size)
-        desired_result = first_row + (middle_row)*(size-2) + last_row
-        for c, neighs in make_neighbors(size=size):
-            for pt in list(neighs):
-                neigh_counts[pt] += 1
+    size = position.size
 
-        assert desired_result == neigh_counts
+    neigh_counts = [0]*(size**2)
+    first_row = result_row(2, size)
+    last_row = result_row(2, size)
+    middle_row = result_row(3, size)
+    desired_result = first_row + (middle_row)*(size-2) + last_row
+    for c, neighs in gd.make_neighbors(size=size):
+        for pt in list(neighs):
+            neigh_counts[pt] += 1
+
+    assert desired_result == neigh_counts
 
 def test_Group_init():
     """Test Initialization of Group object
