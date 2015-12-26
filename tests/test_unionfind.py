bce4355eb59d728fb9c9942b7ce235d19623f3d5
Andrew Brown <salvor7@gmail.com>
2015-12-25 16:49:12 -0800

    Test unlimited unionfind initialization
    Test the unionfind case when there is no limit to it's size. Its size
    limit is numpy infinty, and the pointer set grows as finds are called.

--- tests/test_unionfind.py
+++ tests/test_unionfind.py
@@ -1,17 +1,15 @@
+import numpy as np
 import pytest
-
 from util.unionfind import UnionFind
 
-@pytest.fixture(params=[1, 10, 100, 1000, 10000, 100000])
+@pytest.fixture(params=[1, 10, 100, 1000, 10000, 100000, np.infty])
 def unionfind(request):
     return UnionFind(size_limit=request.param)
 
-@pytest.fixture()
-def unlimited_uf(request):
-    return UnionFind()
-
 def test_uf_init(unionfind):
-    assert len(unionfind._pointers) == unionfind.size_limit
+    if (unionfind.size_limit is np.infty):
+        unionfind[2**16]
+    assert (len(unionfind._pointers) == unionfind.size_limit) or (unionfind.size_limit is np.infty)
     assert len(unionfind) == len(unionfind._pointers)
     for pt in unionfind:
         assert unionfind._pointers[pt] == pt
