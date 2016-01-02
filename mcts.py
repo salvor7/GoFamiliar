422e90852ada36940fac7001ffbf0bdfa95f32a6
Andrew Brown <salvor7@gmail.com>
2016-01-02 13:39:37 -0800

    Play a complete random game
    The search function can now be used play a complete random game. This
    basically covers the basic functionality of the MCTS search function.

--- mcts.py
+++ mcts.py
@@ -1,2 +1,20 @@
+import random
+
+import godata as gd
+
 def search(position):
-    return 0
+    intersections = list(range(0, 19**2-1))
+    random.shuffle(intersections)
+
+    for move in intersections:
+        if position[move] is gd.OPEN_POINT:
+            try:
+                position.check_move(test_pt=move, colour=position.next_player)
+            except gd.MoveError:
+                pass
+            else:
+                break
+    else:
+        raise gd.MoveError('Terminal Position')
+
+    return move
--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -8,4 +8,14 @@ def position():
     return gd.Position()
 
 def test_search(position):
-    assert type(mcts.search(position)) is int
+    move_pt = None
+    for _ in range(19**3):
+        try:
+            move_pt, last_pt = mcts.search(position), move_pt
+        except gd.MoveError:
+            break
+        assert type(move_pt) is int
+        assert move_pt != last_pt
+        position.move(move_pt=move_pt)
+    else:
+        assert 'it is an' == 'infinite loop'
