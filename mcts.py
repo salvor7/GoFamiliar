0aec167b0a25f68588d9171dc7ea0da0901277bd
Andrew Brown <salvor7@gmail.com>
2016-01-02 15:07:53 -0800

    Add eye avoidance logic and tests
    Moves should not be made into eyes, so the search function should
    dismiss eyes as potential moves

--- mcts.py
+++ mcts.py
@@ -1,13 +1,20 @@
 import random
-
 import godata as gd
 
+
+def not_eye(position, move):
+    group_colour = set([group.colour for _, group in position.neigh_groups(move)])
+
+    s = len(group_colour)
+    return group_colour not in [[1] * s, [-1] * s]
+
+
 def search(position):
-    intersections = list(range(0, 19**2-1))
+    intersections = list(range(0, 19 ** 2 - 1))
     random.shuffle(intersections)
 
     for move in intersections:
-        if position[move] is gd.OPEN_POINT:
+        if position[move] is gd.OPEN_POINT and not_eye(position, move):
             try:
                 position.check_move(test_pt=move, colour=position.next_player)
             except gd.MoveError:
--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -1,22 +1,51 @@
 import pytest
-
 import godata as gd
 import mcts
 import tests.test_fixtures as fixt
 
-@pytest.fixture
+
+@pytest.fixture()
 def position():
     return fixt.open_position()()
 
-def test_search(position):
+
+@pytest.fixture()
+def position_moves():
+    return fixt.first_position()(s=19)
+
+
+def test_search_open_board(position, position_moves):
     move_pt = None
-    for _ in range(19**3):
+    for idx in range(19 ** 3):
         try:
             move_pt, last_pt = mcts.search(position), move_pt
-        except gd.MoveError:
+        except gd.MoveError as err:
+            assert str(err) == 'Terminal Position'
             break
         assert type(move_pt) is int
         assert move_pt != last_pt
         position.move(move_pt=move_pt)
     else:
         assert 'it is an' == 'infinite loop'
+    assert idx > 200
+
+
+def test_not_eye(position_moves):
+    position, moves = position_moves
+    group_eyes = [0, 20, 19 ** 2 - 1]
+    for pt in position.board:
+        pt_not_eye = (pt not in group_eyes)
+        assert mcts.not_eye(position, pt) == pt_not_eye
+
+
+def test_search_avoid_eyes(position_moves):
+    position, moves = position_moves
+    move_pt = None
+    for _ in range(19 ** 3):
+        try:
+            move_pt, last_pt = mcts.search(position), move_pt
+        except gd.MoveError as err:
+            assert str(err) == 'Terminal Position'
+            break
+        assert move_pt not in [0, 2]
+        position.move(move_pt=move_pt)
