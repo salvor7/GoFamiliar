e550c8b3d970f903a9aef6aaedd0700ab61da856
Andrew Brown <salvor7@gmail.com>
2016-02-08 10:40:38 -0800

    Remove repeat test
    The original intention of this test is lost to me, so I'm deleting the
    test. Maybe comment tests?

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -110,20 +110,7 @@ def test_search_open_board(position, position_moves):
     assert idx > 200
 
 
-def test_search_avoid_eyes(position_moves):
-    position, moves = position_moves
-    move_pt = None
-    for idx in range(19 ** 2 * 3):
-        try:
-            move_pt, last_pt = mcts.search(position), move_pt
-        except go.MoveError as err:
-            assert str(err) == 'Terminal Position'
-            break
-        assert move_pt not in [0, 20]
-        position.move(move_pt=move_pt)
-    else:
-        assert 'it is an' == 'infinite loop'
-    assert idx > 200
+        print(position.board)
 
 
 @pytest.fixture(scope='module')
