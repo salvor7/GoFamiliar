514b3f6f2e31b3435e3581084c9b78156743671f
Andrew Brown <salvor7@gmail.com>
2016-06-12 14:04:38 -0700

    Add the move_search generating function to the new AnalysisButtonGrid
    The AnalysisButtonGrid is a copy of the ButotnGrid used in the board object, but
    the MCTS node management and tracking is added.
    It may be nessecary to run the algorithm in another process thread to prevent the kivy app from essentially halting.

--- gofamiliar.py
+++ gofamiliar.py
@@ -8,10 +8,14 @@ from kivy.uix.image import Image
 from kivy.logger import Logger
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.clock import Clock
-from kivy.uix.stacklayout import StackLayout
 
 from thick_goban import go
 from mcts import move_search
+
+
+class GoFamiliarApp(App):
+    def build(self):
+        return GoFamiliar()
 
 
 class GoFamiliar(BoxLayout):
@@ -29,23 +33,22 @@ class AnalysisPanel(BoxLayout):
 class Board(FloatLayout):
     background = ObjectProperty(None)
     
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        try:
-            self.method1()
-        except AttributeError:
-            Clock.schedule_once(self.method1)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    def method1(self, inst=None, value=None):
-        # Logger.info(str(self.background.texture))
-        pass
+
+class AnalysisBoard(FloatLayout):
+    background = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class ButtonGrid(GridLayout):
     gamestate = ListProperty([])
 
     def __init__(self, **kwargs):
-        super(ButtonGrid, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         for i in range(19*19):
             self.add_cell(i)
         self.state = go.Position()
@@ -64,7 +67,88 @@ class ButtonGrid(GridLayout):
             instance.lastmove.circle = circle_values(instance)
             instance.lastmove.width = instance.width * 0.05
 
-        # Logger.info('Board state: ' + str(value))
+        Logger.info('Board state: ' + str(value))
+        for inter in self.intersectionlist:
+            inter_colour = self.gamestate[inter.intersection_id]
+
+            if inter_colour == go.BLACK:
+                alpha = 1
+                move_marker_colour = (1, 1, 1, 1)
+                inter.stone_image.source = inter.stone_image.sourceblack
+            elif inter_colour == go.WHITE:
+                alpha = 1
+                move_marker_colour = (0, 0, 0, 1)
+                inter.stone_image.source = inter.stone_image.sourcewhite
+            elif inter_colour == go.OPEN:
+                alpha = 0
+
+            inter.stone_image.color = (1, 1, 1, alpha)
+
+            if inter.intersection_id == self.state.lastmove:
+                with inter.stone_image.canvas.after:
+                    Color(*move_marker_colour)
+                    inter.stone_image.lastmove = Line(
+                        circle=circle_values(inter.stone_image),
+                        width=inter.stone_image.width * 0.05
+                    )
+                    inter.stone_image.bind(pos=_update_marker, size=_update_marker)
+            else:
+                inter.stone_image.canvas.after.clear()
+
+    def add_cell(self, index):
+        def make_move(instance):
+            info_label = App.get_running_app().root.ids._play_panel.ids._move_info_label
+            try:
+                self.state.move(move_pt=instance.intersection_id)
+            except go.MoveError as e:
+                info_label.text = str(e)
+            else:
+                info_label.text = ''
+            self.gamestate = self.state.board._board_colour
+
+        def _update_loc(inst, value):
+            '''Move a child to a new new size/pos. Used by add_cell'''
+            inst.cover.size = inst.size
+            inst.cover.pos = inst.pos
+
+        intersection = Intersection(intersection_id=index)
+        intersection.bind(on_press=make_move)
+        self.add_widget(intersection)
+        try:
+            self.intersectionlist.append(intersection)
+        except AttributeError:
+            Clock.schedule_once(lambda dt: self.intersectionlist.append(intersection))
+
+
+class AnalysisButtonGrid(GridLayout):
+    gamestate = ListProperty([])
+    root_node = ObjectProperty(None)
+    current_node = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(19*19):
+            self.add_cell(i)
+        move_search_gen = move_search(state=Position(), sim_limit=500)
+        self.root_node = next(move_search_gen)
+        self.current_node = self.root_node
+        self.state = current_node.state
+        self.intersectionlist = []
+
+    def on_gamestate(self, instance, value):
+
+        def circle_values(stone_image):
+            x_pos = stone_image.x + stone_image.width / 2
+            y_pos = stone_image.y + stone_image.height / 2
+            radius = 0.3 * stone_image.height
+
+            return x_pos, y_pos, radius
+
+        def _update_marker(instance, value):
+            instance.lastmove.circle = circle_values(instance)
+            instance.lastmove.width = instance.width * 0.05
+
+        Logger.info('Board state: ' + str(value))
         for inter in self.intersectionlist:
             inter_colour = self.gamestate[inter.intersection_id]
 
@@ -131,11 +215,6 @@ class StoneImage(Image):
         
 class BoardImage(Image):
     pass
-
-
-class GoFamiliarApp(App):
-    def build(self):
-        return GoFamiliar()
 
 
 if __name__ == '__main__':
