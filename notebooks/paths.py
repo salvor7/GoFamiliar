f2850da81afb4485a34f379ba7bc09fafb1b9d4b
Andrew Brown <salvor7@gmail.com>
2017-10-08 01:51:05 -0700

    Ensure thick_goban is importable by notebooks

--- notebooks/paths.py
+++ notebooks/paths.py
@@ -5,4 +5,4 @@ Adds source code folder to PYTHONPATH, so imports can be made from notebooks.
 import sys
 import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parents[1] / 'src'))
-sys.path.insert(0, 'C:\Anaconda3\\envs\\openai\\thick_goban\\thick_goban')
+sys.path.insert(0, 'C:\Anaconda3\\envs\\gofamiliar\\thick_goban\\thick_goban')
